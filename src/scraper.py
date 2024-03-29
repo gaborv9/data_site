de9143329ee0a27c870ff41c39d5b20e60788058
Karburator Neun <l@l>
2024-03-29 15:59:01 +0100

    Refactor greenhouse scraper

--- src/scraper.py
+++ src/scraper.py
@@ -5,11 +5,24 @@ import os
 from pathlib import Path
 
 
-def gr_scraper(target_path: Path, gr_token: str) -> None:
+
+
+def get_board_tokens(all_tokens_file_path: Path) -> list:
+    with open(all_tokens_file_path, "r") as file:
+        tokens = file.readlines()
+    tokens = [line.strip() for line in tokens]
+    return tokens
+
+
+def scrape_greenhouse_boards(target_path: Path, board_tokens: list) -> None:
+    for board_token in board_tokens:
+        scrape_greenhouse_board(target_path, board_token)
+
+def scrape_greenhouse_board(target_path: Path, board_token: str) -> None:
     NAME = 'greenhouse'
-    url = 'https://boards-api.greenhouse.io/v1/boards/' + gr_token + '/jobs?content=true'
+    url = 'https://boards-api.greenhouse.io/v1/boards/' + board_token + '/jobs?content=true'
     response = requests.get(url)
-    save_path = target_path / Path(NAME) / str(gr_token + ".json")
+    save_path = target_path / Path(NAME) / str(board_token + ".json")
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
@@ -17,10 +30,13 @@ def gr_scraper(target_path: Path, gr_token: str) -> None:
         with open(save_path, "w") as file:
             json.dump(data, file, indent=4)
 
-        print(f'{gr_token} saved')
+        print(f'{board_token} saved')
     else:
-        print(f'{gr_token} failed: {response.status_code}')
+        print(f'{board_token} failed: {response.status_code}')
 
+# def lever_scraper()
+#     'https://api.eu.lever.co/v0/postings/' + board_token + '?mode=json'
+#
 
 def main() -> None:
     YEAR = '2024'
@@ -31,14 +47,9 @@ def main() -> None:
     root_folder = os.path.dirname(Path(__file__).resolve().parents[0])
     target_path = root_folder / Path(DATA_FOLDER) / Path(YEAR + '_' + MONTH)
 
-    gr_tokens_path = root_folder / Path(GREENHOUSE_TOKENS)
+    greenhouse_board_tokens = get_board_tokens(root_folder / Path(GREENHOUSE_TOKENS))
+    scrape_greenhouse_boards(target_path, greenhouse_board_tokens)
 
-    with open(gr_tokens_path, "r") as file:
-        gr_tokens = file.readlines()
-    gr_tokens_list = [line.strip() for line in gr_tokens]
-
-    for gr_token in gr_tokens_list:
-        gr_scraper(target_path, gr_token)
 
 
 
